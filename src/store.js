87fc99238c79c1c8c0db9a6f0b96f2a122306444
Blackbeard348 <cmorgan348@outlook.com>
2025-03-09 08:19:00 +0800

    practice code

--- src/store.js
+++ src/store.js
@@ -1,9 +1,16 @@
-// store.js
 import { configureStore } from '@reduxjs/toolkit';
 import venueReducer from './venueSlice';
+import avReducer from './avSlice';
+import mealsReducer from './mealsSlice';
+
+console.log('venueReducer:', venueReducer);
+console.log('avReducer:', avReducer);
+console.log('mealsReducer:', mealsReducer);
 
 export default configureStore({
   reducer: {
     venue: venueReducer,
+    av: avReducer,
+    meals: mealsReducer,
   },
 });
