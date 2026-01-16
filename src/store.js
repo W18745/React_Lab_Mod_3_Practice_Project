f1b927f66fb41912c7500e401d2a4279da3356b9
N3T0 <starkodersamm90210@gmail.com>
2026-01-16 14:16:08 -0500

    display selected items in a table

--- src/store.js
+++ src/store.js
@@ -2,10 +2,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import venueReducer from './venueSlice';
 import avReducer from './avSlice';
+import mealsReducer from './mealReducer';
 
 export default configureStore({
   reducer: {
     venue: venueReducer,
     av: avReducer,
+    meals: mealsReducer,
   },
 });
