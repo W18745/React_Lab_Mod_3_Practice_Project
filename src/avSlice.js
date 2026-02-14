4582cc4ad91f63979781a7e747c0695e49b3c82c
anton durov <antondurov05@protonmai.com>
2026-02-14 11:40:26 -0500

    final commit - practice project

--- src/avSlice.js
+++ src/avSlice.js
@@ -4,32 +4,32 @@ export const avSlice = createSlice({
   name: "av",
   initialState: [
         {
-        img: "https://pixabay.com/images/download/business-20031_640.jpg",
+        img: "https://pixabay.com/photos/business-computer-conference-20031/",
         name: "Projectors",
         cost: 200,
         quantity: 0,
     },
     {
-        img: "https://pixabay.com/images/download/speakers-4109274_640.jpg",
+        img: "https://pixabay.com/photos/speakers-bluetooth-tech-speaker-4109274/",
         name: "Speaker",
         cost: 35,
         quantity: 0,
     },
     {
-        img: "https://pixabay.com/images/download/public-speaking-3926344_640.jpg",
+        img: "https://pixabay.com/photos/public-speaking-mic-microphone-3926344/",
         name: "Microphones",
         cost: 45,
         quantity: 0,
     },
     {
-        img: "https://pixabay.com/images/download/whiteboard-2903269_640.png",
+        img: "https://pixabay.com/photos/whiteboard-dry-erase-marker-blank-2903269/",
         name: "Whiteboards",
         cost: 80,
         quantity: 0,
     },
 
     {
-        img: "https://pixabay.com/images/download/signpost-235079_640.jpg",
+        img: "https://pixabay.com/photos/signpost-waypoint-wood-grain-board-235079/",
         name: "Signage",
         cost: 80,
         quantity: 0,
@@ -46,8 +46,8 @@ export const avSlice = createSlice({
         }
     },
     decrementAvQuantity: (state, action) => {
-        const item  = state[action.payload];
-        if (item && item.quantity >0) {
+        const item = state[action.payload];
+        if (item && item.quantity > 0) {
             item.quantity--;
         }
     },
